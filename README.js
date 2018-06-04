70564091e72d3c7bbec44ecf53d8d9c6d20505dd
MegaDoink <39899296+MegaDoink@users.noreply.github.com>
2018-06-04 09:15:05 +0200

    Update README.js

--- README.js
+++ README.js
@@ -1,8 +1,18 @@
-# Small
+<!DOCTYPE html>
+<html>
+<body>
 
-let die = true 
+<h2>What Can JavaScript Do?</h2>
 
-if { (die = false)
-console.log(lev) 
-} else { console.log(dö) 
-  }
+<p>JavaScript can change HTML attribute values.</p>
+
+<p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>
+
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+
+</body>
+</html>
